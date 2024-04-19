cee56d6a315b6f6938ceaada9db409729a0d0d22
GithinjiKelvin <githinjikelvin59@gmail.com>
2024-04-19 23:20:58 +0300

    complete

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+
+function writeCards(names, message){
+    let messages = []
+    for(let name of names){
+        messages.push(`Thank you, ${name}, for the wonderful ${message} gift!`)
+    }
+    return messages
+}
+
+function countDown(num){
+    for(let i=0; i<=num; num-- ){
+        console.log(num);
+    }
+}
